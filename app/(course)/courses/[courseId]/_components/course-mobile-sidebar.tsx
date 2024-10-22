9acda96321824c494bcd2736d3b5fafc1959cd1f
HAMUZA <kiggunduhamuza16@gmail.com>
2024-10-22 14:57:20 +0300

    prod_eduskill

--- app/(course)/courses/[courseId]/_components/course-mobile-sidebar.tsx
+++ app/(course)/courses/[courseId]/_components/course-mobile-sidebar.tsx
@@ -1,3 +1,4 @@
+// Import statements remain unchanged
 import { Menu } from "lucide-react";
 import { Chapter, Course, UserProgress } from "@prisma/client";
 
@@ -16,24 +17,26 @@ interface CourseMobileSidebarProps {
     })[];
   };
   progressCount: number;
+  quizId: string; // Add quizId to the props interface
 };
 
 export const CourseMobileSidebar = ({ 
   course,
   progressCount,
+  quizId, // Accept quizId as a prop
 }: CourseMobileSidebarProps) => {
-
   
   return (
     <Sheet>
       <SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition">
         <Menu />
       </SheetTrigger>
-      <SheetContent side="left" className="p-0  w-72">
+      <SheetContent side="left" className="p-0 w-72">
         <CourseSidebar
           course={course}
           progressCount={progressCount}
-         />
+          quizId={quizId} // Pass quizId to CourseSidebar
+        />
       </SheetContent>
     </Sheet>
   )
