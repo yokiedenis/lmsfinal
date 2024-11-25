55340f75ca494f4458d87715951bef91b17fa1a0
hamuzah <37175583+hamuzah@users.noreply.github.com>
2024-11-25 14:54:37 +0300

    Update reportWebVitals.ts

--- agora-test/src/reportWebVitals.ts
+++ agora-test/src/reportWebVitals.ts
@@ -1,16 +1,14 @@
-import  ReportHandler  from 'web-vitals';
+import { getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
 
 type ReportHandler = (metric: { name: string; delta: number; id: string; }) => void;
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    getCLS(onPerfEntry);
+    getFID(onPerfEntry);
+    getFCP(onPerfEntry);
+    getLCP(onPerfEntry);
+    getTTFB(onPerfEntry);
   }
 };
 
