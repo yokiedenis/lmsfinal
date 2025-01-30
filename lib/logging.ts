45e191c755c9d3d41393ffb03388d2ba38c80564
HAMUZA <kiggunduhamuza16@gmail.com>
2025-01-30 17:45:55 +0300

    Eduskill

--- lib/logging.ts
+++ lib/logging.ts
@@ -4,12 +4,13 @@ export interface Logging {
     id?: string;
     url: string;
     method: string;
+    userId: string;  // Added userId
     body?: string;
     response?: string;
     statusCode?: number;
     errorMessage?: string;
     createdAt: Date;
-  }
+}
 
 export async function createLogging(logging: Logging): Promise<void> {
   await db.logging.create({
