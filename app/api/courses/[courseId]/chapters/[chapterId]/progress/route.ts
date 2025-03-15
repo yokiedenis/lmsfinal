b683cd198eb2739295bc5a7d5c417ad58fe32252
HAMUZA <kiggunduhamuza16@gmail.com>
2025-03-15 08:15:10 +0300

    Eduskill_final

--- app/api/courses/[courseId]/chapters/[chapterId]/progress/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/progress/route.ts
@@ -1,6 +1,47 @@
+// import { auth } from "@clerk/nextjs/server";
+// import { NextResponse } from "next/server";
+
+// import { db } from "@/lib/db";
+
+// export async function PUT(
+//   req: Request,
+//   { params }: { params: { courseId: string; chapterId: string } }
+// ) {
+//   try {
+//     const { userId } = auth();
+//     const { isCompleted } = await req.json();
+
+//     if (!userId) {
+//       return new NextResponse("Unauthorized", { status: 401 });
+//     } 
+
+//     const userProgress = await db.userProgress.upsert({
+//       where: {
+//         userId_chapterId: {
+//           userId,
+//           chapterId: params.chapterId,
+//         }
+//       },
+//       update: {
+//         isCompleted
+//       },
+//       create: {
+//         userId,
+//         chapterId: params.chapterId,
+//         isCompleted,
+//       }
+//     })
+
+//     return NextResponse.json(userProgress);
+//   } catch (error) {
+//     console.log("[CHAPTER_ID_PROGRESS]", error);
+//     return new NextResponse("Internal Error", { status: 500 });
+//   }
+// }
+
+
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-
 import { db } from "@/lib/db";
 
 export async function PUT(
@@ -13,7 +54,7 @@ export async function PUT(
 
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
-    } 
+    }
 
     const userProgress = await db.userProgress.upsert({
       where: {
@@ -23,14 +64,15 @@ export async function PUT(
         }
       },
       update: {
-        isCompleted
+        isCompleted,
       },
       create: {
         userId,
         chapterId: params.chapterId,
         isCompleted,
+        courseId: params.courseId, // Include courseId here
       }
-    })
+    });
 
     return NextResponse.json(userProgress);
   } catch (error) {
