4d300524b244b16e9e0807c4790e722381cec774
HAMUZA <kiggunduhamuza16@gmail.com>
2025-03-15 07:36:33 +0300

    Eduskill_final

--- app/api/courses/[courseId]/fetchattach/route.ts
+++ app/api/courses/[courseId]/fetchattach/route.ts
@@ -1,27 +1,55 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+// import { NextApiRequest, NextApiResponse } from 'next';
+// import { db } from "@/lib/db";
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   const { courseId, attachId } = req.query;
+
+//   if (req.method === 'GET') {
+//     try {
+//       const attachment = await db.attachment.findUnique({
+//         where: {
+//           id: attachId as string,
+//           courseId: courseId as string,
+//         },
+//       });
+
+//       if (attachment) {
+//         return res.status(200).json(attachment);
+//       } else {
+//         return res.status(404).json({ error: 'Attachment not found' });
+//       }
+//     } catch (error) {
+//       return res.status(500).json({ error: 'Internal Server Error' });
+//     }
+//   } else {
+//     res.setHeader('Allow', ['GET']);
+//     return res.status(405).end(`Method ${req.method} Not Allowed`);
+//   }
+// }
+
+
+
+
+
+import { NextResponse } from 'next/server';
 import { db } from "@/lib/db";
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { courseId, attachId } = req.query;
 
-  if (req.method === 'GET') {
-    try {
-      const attachment = await db.attachment.findUnique({
-        where: {
-          id: attachId as string,
-          courseId: courseId as string,
-        },
-      });
+export async function GET(req: Request, { params }: { params: { courseId: string; attachId: string } }) {
+  const { courseId, attachId } = params;
 
-      if (attachment) {
-        return res.status(200).json(attachment);
-      } else {
-        return res.status(404).json({ error: 'Attachment not found' });
-      }
-    } catch (error) {
-      return res.status(500).json({ error: 'Internal Server Error' });
+  try {
+    const attachment = await db.attachment.findUnique({
+      where: {
+        id: attachId,
+        courseId: courseId,
+      },
+    });
+
+    if (attachment) {
+      return NextResponse.json(attachment);
+    } else {
+      return NextResponse.json({ error: 'Attachment not found' }, { status: 404 });
     }
-  } else {
-    res.setHeader('Allow', ['GET']);
-    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  } catch (error) {
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
   }
 }
